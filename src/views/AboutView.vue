540b4d8e752d3dd37fd00cf39f09c00499782dc4
tom.fjeldheim <tom.fjeldheim@gmail.com>
2023-05-22 12:15:37 +0200

    bjorn_learning

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+
+    <Besttable></Besttable>
   </div>
 </template>
+
+<script>
+import Besttable from "@/components/testcomponents/epictable.vue";
+
+export default {
+  components: {
+    Besttable,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>{{ test }}</p>
+    <p v-if="isLoggedin == true">heihei</p>
+    <v-textarea v-model="test"></v-textarea>
+    <v-row>
+      <v-col
+        cols="4"
+        v-for="(person, personIndex) in people"
+        :key="personIndex"
+      >
+        <v-card class="pa-5 ma-5" v-if="person.age > 28">
+          <p>{{ person.name }}</p>
+        </v-card>
+      </v-col>
+      <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12">
+        <p>Hei</p>
+      </v-col>
+      <v-col cols="6" v-if="$vuetify.breakpoint.mdAndDown">
+        <p>Hei</p>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld";
 
 export default {
-  name: 'HomeView',
+  name: "Home",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      test: "",
+      isLoggedin: false,
+      people: [
+        {
+          name: "bob",
+          age: 29,
+        },
+        {
+          name: "per",
+          age: 99,
+        },
+      ],
+    };
+  },
+};
 </script>
